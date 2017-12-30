1e2b2df14a0e8a93f05f8e7fea6b5651aa3fbd64
Jayashankar Jayan <20151948+jayashankarjayan@users.noreply.github.com>
2017-12-30 16:17:18 +0530

    Responsive issues fixed
    Cleaned up code and fixed responsive issues

--- make-a-button.php
+++ make-a-button.php
@@ -36,6 +36,8 @@ $_SESSION['mainColor'] = $cin;
 	{
 		$link = $_GET['link'];		
 	}
+
+	// Generate button pressed
 	if(isset($_POST['generate']))
 	{
 		$generated = true;
@@ -62,8 +64,10 @@ $_SESSION['mainColor'] = $cin;
 	function checkLink($link)
 	{
 		
+		// Check if query value is a url
 		if (preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$link))
 		{
+			// Check if link is falling under the domain
  			 if(preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[devdevcredits.herokuapp.com]/",$link))
  			 {
  			 	return true;
@@ -83,52 +87,61 @@ $_SESSION['mainColor'] = $cin;
 	{
 		switch ($color) 
     	{
+    		// Dark Color
     		case 'dark': ?>
-    			<xmp><a href="<?php echo $link; ?>" class="btn btn-outline-dark <?php echo $size; ?>" role="button" aria-disabled="true"><?php echo $buttonText; ?></a></xmp>
+    			<input type="text" value='<a href="<?php echo $link; ?>" class="btn btn-outline-dark <?php echo $size; ?>" role="button" target="_blank" aria-disabled="true"><?php echo $buttonText; ?></a>' class="generated-button-link" readonly/ >
+
 	        	<p>Your button will look like this</p><br/>
-	        	<a href="<?php echo $link; ?>" class="btn btn-outline-dark <?php echo $size; ?>" role="button" aria-disabled="true"><?php echo $buttonText; ?></a>
+	        	<a href="<?php echo $link; ?>" class="btn btn-outline-dark <?php echo $size; ?>" role="button" target="_blank" aria-disabled="true"><?php echo $buttonText; ?></a>
     			<?php break;
 
+    		// Blue Color
     		case 'blue': ?>
-    			<xmp><a href="<?php echo $link; ?>" class="btn btn-outline-primary <?php echo $size; ?>" role="button" aria-disabled="true"><?php echo $buttonText; ?></a></xmp>
+    			<input type="text" value='<a href="<?php echo $link; ?>" class="btn btn-outline-primary <?php echo $size; ?>" role="button" target="_blank" aria-disabled="true"><?php echo $buttonText; ?></a>' class="generated-button-link" readonly/>
 	        	<p>Your button will look like this</p><br/>
-	        	<a href="<?php echo $link; ?>" class="btn btn-outline-primary <?php echo $size; ?>" role="button" aria-disabled="true"><?php echo $buttonText; ?></a>
+	        	<a href="<?php echo $link; ?>" class="btn btn-outline-primary <?php echo $size; ?>" role="button" target="_blank" aria-disabled="true"><?php echo $buttonText; ?></a>
     			<?php break;
 
+    		// Red Color
     		case 'red': ?>
-    			<xmp><a href="<?php echo $link; ?>" class="btn btn-outline-danger <?php echo $size; ?>" role="button" aria-disabled="true"><?php echo $buttonText; ?></a></xmp>
+    			<input type="text" value='<a href="<?php echo $link; ?>" class="btn btn-outline-danger <?php echo $size; ?>" role="button" target="_blank" aria-disabled="true"><?php echo $buttonText; ?></a>' class="generated-button-link" readonly/>
 	        	<p>Your button will look like this</p><br/>
-	        	<a href="<?php echo $link; ?>" class="btn btn-outline-danger <?php echo $size; ?>" role="button" aria-disabled="true"><?php echo $buttonText; ?></a>
+	        	<a href="<?php echo $link; ?>" class="btn btn-outline-danger <?php echo $size; ?>" role="button" target="_blank" aria-disabled="true"><?php echo $buttonText; ?></a>
     			<?php break;
 
+    		// Green Color
     		case 'green': ?>
-    			<xmp><a href="<?php echo $link; ?>" class="btn btn-outline-success <?php echo $size; ?>" role="button" aria-disabled="true"><?php echo $buttonText; ?></a></xmp>
+    			<input type="text" value='<a href="<?php echo $link; ?>" class="btn btn-outline-success <?php echo $size; ?>" role="button" target="_blank" aria-disabled="true"><?php echo $buttonText; ?></a>' class="generated-button-link" readonly/>
 	        	<p>Your button will look like this</p><br/>
-	        	<a href="<?php echo $link; ?>" class="btn btn-outline-success <?php echo $size; ?>" role="button" aria-disabled="true"><?php echo $buttonText; ?></a>
+	        	<a href="<?php echo $link; ?>" class="btn btn-outline-success <?php echo $size; ?>" role="button" target="_blank" aria-disabled="true"><?php echo $buttonText; ?></a>
     			<?php break;
 
+    		// Grey color
     		case 'grey': ?>
-    			<xmp><a href="<?php echo $link; ?>" class="btn btn-outline-secondary btn-lg" role="button" aria-disabled="true"><?php echo $buttonText; ?></a></xmp>
+    			<input type="text" value='<a href="<?php echo $link; ?>" class="btn btn-outline-secondary <?php echo $size; ?>" role="button" target="_blank" aria-disabled="true"><?php echo $buttonText; ?></a>' class="generated-button-link" readonly/>
 	        	<p>Your button will look like this</p><br/>
-	        	<a href="<?php echo $link; ?>" class="btn btn-outline-secondary <?php echo $size; ?>" role="button" aria-disabled="true"><?php echo $buttonText; ?></a>
+	        	<a href="<?php echo $link; ?>" class="btn btn-outline-secondary <?php echo $size; ?>"target="_blank"  role="button" aria-disabled="true"><?php echo $buttonText; ?></a>
     			<?php break;
 
+    		// Orange Color
     		case 'orange': ?>
-    			<xmp><a href="<?php echo $link; ?>" class="btn btn-outline-warning <?php echo $size; ?>" role="button" aria-disabled="true"><?php echo $buttonText; ?></a></xmp>
+    			<input type="text" value='<a href="<?php echo $link; ?>" class="btn btn-outline-warning <?php echo $size; ?>" role="button" target="_blank" aria-disabled="true"><?php echo $buttonText; ?></a>' class="generated-button-link" readonly/>
 	        	<p>Your button will look like this</p><br/>
-	        	<a href="<?php echo $link; ?>" class="btn btn-outline-warning <?php echo $size; ?>" role="button" aria-disabled="true"><?php echo $buttonText; ?></a>
+	        	<a href="<?php echo $link; ?>" class="btn btn-outline-warning <?php echo $size; ?>" target="_blank" role="button" aria-disabled="true"><?php echo $buttonText; ?></a>
     			<?php break;
 
+    		//  White/light color
     		case 'white': ?>
-    			<xmp><a href="<?php echo $link; ?>" class="btn btn-outline-light <?php echo $size; ?>" role="button" aria-disabled="true"><?php echo $buttonText; ?></a></xmp>
+    			<input type="text" value='<a href="<?php echo $link; ?>" class="btn btn-outline-white <?php echo $size; ?>" role="button" target="_blank" aria-disabled="true"><?php echo $buttonText; ?></a>' class="generated-button-link" readonly/>
 	        	<p>Your button will look like this</p><br/>
-	        	<a href="<?php echo $link; ?>" class="btn btn-outline-white <?php echo $size; ?>" role="button" aria-disabled="true"><?php echo $buttonText; ?></a>
+	        	<a href="<?php echo $link; ?>" class="btn btn-outline-white <?php echo $size; ?>" target="_blank" role="button" aria-disabled="true"><?php echo $buttonText; ?></a>
     			<?php break;
     		
+    		// Default is dark color
     		default: ?>
-    			<xmp><a href="<?php echo $link; ?>" class="btn btn-outline-dark <?php echo $size; ?>" role="button" aria-disabled="true"><?php echo $buttonText; ?></a></xmp>
+    			<input type="text" value='<a href="<?php echo $link; ?>" class="btn btn-outline-dark <?php echo $size; ?>" role="button" target="_blank" aria-disabled="true"><?php echo $buttonText; ?></a>' class="generated-button-link" readonly/>
 	        	<p>Your button will look like this</p><br/>
-	        	<a href="<?php echo $link; ?>" class="btn btn-outline-dark <?php echo $size; ?>" role="button" aria-disabled="true"><?php echo $buttonText; ?></a>
+	        	<a href="<?php echo $link; ?>" class="btn btn-outline-dark <?php echo $size; ?>" target="_blank" role="button" aria-disabled="true"><?php echo $buttonText; ?></a>
     			<?php break;
     	}
 	}
@@ -153,104 +166,103 @@ $_SESSION['mainColor'] = $cin;
 <link rel="stylesheet" href="/style.css">
 
 <!-- Use set color to dynamically change the theme -->
-<link rel="stylesheet" type="text/css" href="style.php">
+<link rel="stylesheet" type="text/css" href="/style.php">
 
 </head>
 
 <body>
-
-<div class="container ">
-	<div class="row">
-	  <div class=" mx-auto">
-	    <div class="card maincard col-ldg-12 col-md-12 col-sm-12 col-xs-12">
-	      <div class="card-body">
-	        <h1 class="card-title text-center"><?php echo $title ?></h1>
-	        <div class="card-text text-center">
-	        	<?php echo $stext; ?>
-	        </div>
-	        
-	        <?php if($generated): ?>
-	        	<?php if($validLink): ?>
-				<div class="alert alert-success" role="alert">
-				  Button generated
-				</div>
-				<?php else: ?>
-	        	<div class="alert alert-danger" role="alert">
-				  Invalid url provided
-				</div>
-	        	<?php endif; ?>
-	        <?php endif; ?>
-	        <div class="form-control input-content">
-	        	<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
-	        		<div class="row">
-					  <div class="col">
-					  	<label for="button-text">Button title</label>
-					    <input type="text" class="form-control" id="button-text" name="button-text" aria-describedby="button-text-help" placeholder="Enter text" required>
-					    <small id="button-text-help" class="form-text text-muted">Enter the text that you want on your button</small>
-					  </div>
-					  <div class="col">
-					  	<label for="button-color">Choose color</label>
-					    <select class="form-control" id="button-color" name="button-color" required>
-					      <option value=""> -- Choose color -- </option>
-					      <option value="dark">Dark</option>
-					      <option value="blue">Blue</option>
-					      <option value="red">Red</option>
-					      <option value="green">Green</option>
-					      <option value="grey">Grey</option>
-					      <option value="orange">Orange</option>
-					      <option value="white">White</option>
-					    </select>
-					  </div>
-					  <div class="col">
-					  	<label for="button-size">Choose button size</label>
-					    <select class="form-control" id="button-size" name="button-size" required>
-					      <option value=""> -- Choose size -- </option>
-					      <option value="btn-sm">Small</option>
-					      <option value="btn-lg">Large</option>
-					      <input type="hidden" name="link" value="<?php echo $link; ?>"></input>
-					    </select>
-					  </div>
-	        			
-	        		</div>
-				  <input type="submit" name="generate" class="btn btn-dark" value="Generate"/>
-				</form>
-	        </div>
-
-	        <?php if($generated): ?>
-	        	<?php if($validLink): ?>
-		        <div class="generated-content text-center">
-		        	<code>Paste the following code in your html to get your button working</code>
-		        	<?php
-		        	button($hrefValue, $buttonText,$buttonColor,$buttonSize); 
-		        	?>		        	
-		        	<br/>
+	<div class="container ">
+		<div class="row">
+		  <div class=" mx-auto">
+		    <div class="card maincard col-ldg-12 col-md-12 col-sm-12 col-xs-12">
+		      <div class="card-body">
+		        <h1 class="card-title text-center"><?php echo $title ?></h1>
+		        <div class="card-text text-center">
+		        	<?php echo $stext; ?>
 		        </div>
-		    <?php endif; ?>
-		     <?php endif; ?>
-	      </div>
-	    </div>
-	  </div>	
-	</div>	
-</div>
-
-
-<!-- <div class="flex one three-1000 demo">
-	<div><span></span></div>
-	<div class="maincard">
-			<h1><?php echo $title ?></h1>
-			<h4><?php echo $stext ?></h4>
-			<br />
-			<div class="credits">Made with &#9825; by</div>
-			<hr />
-			<div class="flex two demo">
-<?php foreach ($users as $user):?>
-				<a href='https://devrant.com/users/<?=$user?>'>
-					<div class='user'>
-						<span><h2><?=$user?></h2></span>
+		        
+		        <!--  Alert comes here -->
+		        <?php if($generated): ?>
+		        	<?php if($validLink): ?>
+		        		<!-- If link is valid -->
+					<div class="alert alert-success" role="alert">
+					  Button generated
 					</div>
-				</a>
-<?php endforeach; ?>
-			</div>
-		</div>
-</div> -->
+					<?php else: ?>
+						<!-- If link is not valid -->
+		        	<div class="alert alert-danger" role="alert">
+					  Invalid url provided
+					</div>
+		        	<?php endif; ?>
+		        <?php endif; ?>
+		        <!-- Alert end -->
+
+		        <!-- Form start -->
+		        <div class="form-control input-content">
+		        	<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+		        		<div class="row">
+		        		
+		        		<!-- Text to be displayed on button start -->
+						 <div class="col-md-4 col-sm-12 col-xs-12">
+						  	<label for="button-text">Button title</label>
+						    <input type="text" class="form-control" id="button-text" name="button-text" aria-describedby="button-text-help" placeholder="Enter text" required>
+						    <small id="button-text-help" class="form-text text-muted">Enter the text that you want on your button</small>
+						 </div>
+						 <!-- Text to be displayed on button end -->
+
+						 <!-- Button color start -->
+						  <div class="col-md-4 col-sm-12 col-xs-12">
+						  	<label for="button-color">Choose color</label>
+						    <select class="form-control" id="button-color" name="button-color" required>
+						      <option value=""> -- Choose color -- </option>
+						      <option value="dark">Dark</option>
+						      <option value="blue">Blue</option>
+						      <option value="red">Red</option>
+						      <option value="green">Green</option>
+						      <option value="grey">Grey</option>
+						      <option value="orange">Orange</option>
+						      <option value="white">White</option>
+						    </select>
+						  </div>
+						  <!-- Button color end -->
+
+						  <!-- Button size start -->
+						  <div class="col-md-4 col-sm-12 col-xs-12">
+						  	<label for="button-size">Choose button size</label>
+						    <select class="form-control" id="button-size" name="button-size" required>
+						      <option value=""> -- Choose size -- </option>
+						      <option value="btn-sm">Small</option>
+						      <option value="btn-lg">Large</option>
+						      <input type="hidden" name="link" value="<?php echo $link; ?>"></input>
+						    </select>
+						  </div>
+		        		<!-- Button color end -->
+
+		        		</div>
+
+		        		<!-- Submit button -->
+					  <input type="submit" name="generate" class="btn btn-dark" value="Generate"/>
+					</form>
+		        </div>
+		        <!-- Form end -->
+
+		        <!-- Generated link and sample start -->
+		        <?php if($generated): ?>
+		        	<?php if($validLink): ?>
+			        <div class="generated-content text-center">
+			        	<code>Paste the following code in your html to get your button working</code>
+				        	<?php
+				        	button($hrefValue, $buttonText,$buttonColor,$buttonSize); 
+				        	?>
+			        </div>
+			        <small>Note: You need to have Bootstrap CSS files linked to your page via CDN or locally for the effects</small>
+			    	<?php endif; ?>
+			     <?php endif; ?>
+			    <!-- Generated link and sample end -->
+
+		      </div>
+		    </div>
+		  </div>	
+		</div>	
+	</div>
 </body>

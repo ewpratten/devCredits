993f27ec6486888d1814ef0af3e7c2f4a267143a
Andre Julius <norom@jimileibniz.de>
2018-01-01 01:45:18 +0100

    Change tabs to spaces. Improve code style.

--- style.php
+++ style.php
@@ -1,59 +1,69 @@
 <?php
-    header("Content-type: text/css; charset: UTF-8");
+header("Content-type: text/css; charset: UTF-8");
+//colors:
+//1 - purple
+//2 - green
+//3 - blue
+//4 - red
+//5 - yellow
+//6 - orange
 
-	$colors = array(1 => '#a872a3',
-				2 => '#7cc8a2',
-				3 => '#2a8b9d',
-				4 => '#d55161',
-				5 => '#ecd175',
-				6 => '#f99a66');
-	//colors:
-	//1 - purple
-	//2 - green
-	//3 - blue
-	//4 - red
-	//5 - yellow
-	//6 - orange
-	
-	$hoverc = array(0 => '#000000',
-				1 => '#69306d',
-				2 => '#eb9486',
-				3 => '#90c2e7',
-				4 => '#fe64a3',
-				5 => '#ff7f11',
-				6 => '#57b8ff');
+$colors = array(
+    1 => '#a872a3',
+    2 => '#7cc8a2',
+    3 => '#2a8b9d',
+    4 => '#d55161',
+    5 => '#ecd175',
+    6 => '#f99a66'
+);
+
+$hover_colors = array(
+    0 => '#000000',
+    1 => '#69306d',
+    2 => '#eb9486',
+    3 => '#90c2e7',
+    4 => '#fe64a3',
+    5 => '#ff7f11',
+    6 => '#57b8ff'
+);
 
 
-	session_start();
-    $cin = $_SESSION['mainColor'];
-    $bgIsAnimated = $_SESSION['bgIsAnimated'];
+session_start();
+$cin = $_SESSION['mainColor'];
+$bgIsAnimated = $_SESSION['bgIsAnimated'];
 
-	if (array_key_exists($cin, $colors)) {
-		$color = $colors[$cin];
-		$hover = $hoverc[$cin];
-	} else {
-		$color = $colors[1];
-		$hover = $hoverc[0];
-	}
-	//templates from coolors.co
-	
-	//use $color to create themes
+if (array_key_exists($cin, $colors)) {
+    $color = $colors[$cin];
+    $hover = $hover_colors[$cin];
+} else {
+    $color = $colors[1];
+    $hover = $hover_colors[0];
+}
+//templates from coolors.co
+
+//use $color to create themes
 ?>
 
 body{
-	background-color: <?php echo $color; ?>;
-	<?php
-if ($bgIsAnimated == 'false') {echo "";} else {
- echo "-webkit-animation-name: cycle-banner; /* Chrome, Safari, Opera */ -webkit-animation-duration: 15s; /* Chrome, Safari, Opera */ -webkit-animation-iteration-count: infinite; /* Chrome, Safari, Opera */ animation-name: cycle-banner; animation-duration: 15s; animation-iteration-count: infinit; animation-timing-function:ease-out;"; }
-	?>
+    background-color: <?php echo $color; ?>;
+    <?php
+    if ($bgIsAnimated == 'false') {
+        echo "";
+    } else {
+        echo "-webkit-animation-name: cycle-banner; /* Chrome, Safari, Opera */ -webkit-animation-duration: 15s; /* Chrome, Safari, Opera */ -webkit-animation-iteration-count: infinite; /* Chrome, Safari, Opera */ animation-name: cycle-banner; animation-duration: 15s; animation-iteration-count: infinit; animation-timing-function:ease-out;";
+    }
+    ?>
 }
+
 .user:hover {
-	 background-color: <?php echo $hover; ?>;
+    background-color: <?php echo $hover; ?>;
     color: #FFFFFF;
 }
+
 a {
-	color:<?php echo $color; ?>;
+    color: <?php echo $color; ?>;
 }
+
 a.dcc:hover {
     color: <?php echo $color; ?>;
 }
